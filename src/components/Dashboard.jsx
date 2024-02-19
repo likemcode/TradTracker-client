c618259c5d32ce8b139a8bf0f6a0d1c1718d855f
josue Avuh <jkomi2003@gmail.com>
2024-02-19 14:59:36 +0100

    Building the dashboard component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
-import { Statistic } from 'antd'
+import { Row,Col,Card,Statistic } from 'antd'
 
 const Dashboard = () => {
   return (
-    <div>
-      Dashoard
-    </div>
+    <>
+      <Row>
+      <div className='metrics-row'>
+        <Card>
+          <Statistic title="Active Users" value={1000} />
+        </Card>
+        <Card>
+          <Statistic title="Revenue" value={25000} prefix="$" />
+        </Card>
+        <Card>
+          <Statistic title="Conversion Rate" value={70} suffix="%" />
+        </Card>
+        <Card>
+          <Statistic title="Progress" value={80} suffix="%" />
+        </Card>
+        <Card>
+          <Statistic title="Progress" value={80} suffix="%" />
+        </Card>
+        </div>
+      </Row>
+      <Row>
+
+      </Row>
+      <Row>
+        
+      </Row>
+    </>
   )
 }
 
