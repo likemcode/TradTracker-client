8e1739fcddfd7ea27da08d1d4d9651ba9972928d
josue Avuh <jkomi2003@gmail.com>
2024-02-20 13:17:12 +0100

    updated the dashboard component (added radio Buttons but might change it to select) , and modified titles for the first row

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,22 +1,45 @@
-import React from 'react'
-import { Row,Col,Card,Statistic } from 'antd'
+import React, {useState} from 'react'
+import { Row,Col,Card,Statistic,Radio, Flex  } from 'antd'
+
 
 const Dashboard = () => {
+  
+  const [selectedPeriod, setSelectedPeriod] =useState('daily');
+  const handlePeriodChange = (e) => {
+    setSelectedPeriod(e.target.value);
+  };
   return (
     <>
+    <Row>
+      <Col span={24}>
+      <Flex  justify="start" align="center" gap='middle'>
+            <Radio.Group
+              value={selectedPeriod}
+              onChange={handlePeriodChange}
+              style={{ gap: '10px' }}
+            >
+              <Radio.Button value="daily"  style={{ margin: '5px',borderRadius: '10px' }}>Daily</Radio.Button>
+              <Radio.Button value="weekly"style={{ margin: '5px',borderRadius: '10px' }}>Weekly</Radio.Button>
+              <Radio.Button value="monthly" style={{ margin: '5px',borderRadius: '10px' }}>Monthly</Radio.Button>
+              <Radio.Button value="yearly" style={{ margin: '5px' ,borderRadius: '10px'}}>Yearly</Radio.Button>
+            </Radio.Group>
+          </Flex>
+      </Col>
+      
+    </Row>
       <Row>
       <div className='metrics-row'>
         <Card>
-          <Statistic title="Active Users" value={1000} />
+          <Statistic title="Balance" value={1000} prefix='$' />
         </Card>
         <Card>
-          <Statistic title="Revenue" value={25000} prefix="$" />
+          <Statistic title="Profit/Loss" value={25000} prefix="$" />
         </Card>
         <Card>
-          <Statistic title="Conversion Rate" value={70} suffix="%" />
+          <Statistic title="Win Rate" value={70} suffix="%" />
         </Card>
         <Card>
-          <Statistic title="Progress" value={80} suffix="%" />
+          <Statistic title="Avg Ratio" value={8} suffix="R" />
         </Card>
         <Card>
           <Statistic title="Progress" value={80} suffix="%" />
