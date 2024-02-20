495c0f96ccf73bb446f58f487511ccd4e08cf099
josue Avuh <jkomi2003@gmail.com>
2024-02-20 13:32:05 +0100

    started building history component

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
-
+import { Table } from 'antd'
 const History = () => {
+  const dataSource = [
+    {
+      key: '1',
+      name: 'Mike',
+      age: 32,
+      address: '10 Downing Street',
+    },
+    {
+      key: '2',
+      name: 'John',
+      age: 42,
+      address: '10 Downing Street',
+    },
+  ];
+  
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
+  
+  
   return (
-    <div>
-      History
-    </div>
+    <>
+    <Table dataSource={dataSource} columns={columns} />;
+    </>
   )
 }
 
