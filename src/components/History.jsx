86b5643f5fa7b376a8ddadc050198aabee2f6f0f
josue Avuh <jkomi2003@gmail.com>
2024-02-21 13:34:27 +0100

    built history component (table with data from the backend displayed)

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,45 +1,76 @@
-import React from 'react'
-import { Table } from 'antd'
+import React, { useState, useEffect } from 'react';
+import { Table , Tag} from 'antd';
+
 const History = () => {
-  const dataSource = [
-    {
-      key: '1',
-      name: 'Mike',
-      age: 32,
-      address: '10 Downing Street',
-    },
-    {
-      key: '2',
-      name: 'John',
-      age: 42,
-      address: '10 Downing Street',
-    },
-  ];
-  
+  const [data, setData] = useState([]);
+
+  // Simulate fetching data from the backend
+  useEffect(() => {
+    const fetchData = async () => {
+      // Replace this with your actual API call
+      const response = await fetch('http://127.0.0.1:8000/backend/trades/list');
+      console.log(response)
+      const result = await response.json();
+      setData(result);
+    };
+
+    fetchData();
+  }, []);
+
+  // Define the columns for the table
   const columns = [
     {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
     },
     {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
+      title: 'Date',
+      dataIndex: 'dates',
+      key: 'dates',
+      render: (text) => new Date(text).toLocaleString(),
     },
     {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
+      title: 'Symbol',
+      dataIndex: 'symbol',
+      key: 'symbol',
+    },
+    {
+      title: 'Buy/Sell',
+      dataIndex: 'buy_sell',
+      key: 'buy_sell',
+      render: (text) => (text === '1' ? 'Buy' : 'Sell'),
+    },
+    {
+      title: 'Volume',
+      dataIndex: 'volume',
+      key: 'volume',
+    },
+    {
+      title: 'Profit',
+      dataIndex: 'profit',
+      key: 'profit',
+      render: (text) => (
+        <Tag color={text >=  0 ? 'green' : 'red'}>
+          {text >=  0 ? `+${text}` : text} $
+        </Tag>
+      )
+    },
+    {
+      title: 'User',
+      dataIndex: 'user',
+      key: 'user',
     },
   ];
-  
-  
-  return (
-    <>
-    <Table dataSource={dataSource} columns={columns} />;
-    </>
-  )
-}
 
-export default History
+  return (
+    <Table
+      columns={columns}
+      dataSource={data}
+      rowKey="id"
+      pagination={{ pageSize: 30 }}
+    />
+  );
+};
+
+export default History;
