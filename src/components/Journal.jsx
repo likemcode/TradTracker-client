0d64d0d6ba7c84dd74e317fb25d4365d4ebf3709
josue Avuh <jkomi2003@gmail.com>
2024-03-21 10:45:16 +0100

    profile update

--- src/components/Journal.jsx
+++ src/components/Journal.jsx
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
-import { Avatar, List, Button , Spin, Tag} from 'antd';
+import React, { useState } from 'react';
+import { Avatar, List, Button, Spin, Tag, Flex } from 'antd';
 import { useNavigate } from 'react-router-dom';
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useGetJournalListQuery } from '../services/BackendApi';
 import NewJournalModal from './NewJournalModal';
 import { SmileOutlined, FrownOutlined, MehOutlined } from '@ant-design/icons'; // Import emoji icons
@@ -13,16 +13,16 @@ const Journal = () => {
 
   const handleCreateJournal = (values) => {
     const { title, symbol, buySell, experience, date } = values;
-   const newId = journals.length > 0 ? Math.max(...journals.map(journal => journal.id)) + 1 : 1;
-   // Perform action to create new journal entry with the provided title
-   console.log('Creating journal entry with title:', title);
-   setModalVisible(false); // Close the modal after creating the entry
-   
-   navigate(`/JournalDetails/${newId}`,  { state: { ...values, date: date.format('YYYY-MM-DD') } }) ;
- };
+    const newId = journals.length > 0 ? Math.max(...journals.map(journal => journal.id)) + 1 : 1;
+    // Perform action to create new journal entry with the provided title
+    console.log('Creating journal entry with title:', title);
+    setModalVisible(false); // Close the modal after creating the entry
+
+    navigate(`/JournalDetails/${newId}`, { state: { ...values, date: date.format('YYYY-MM-DD') } });
+  };
 
   if (isLoading) {
-    return <div><Spin/></div>;
+    return <div><Spin /></div>;
   }
 
   if (error) {
@@ -30,47 +30,51 @@ const Journal = () => {
   }
 
   return (
-   <div>
-   {/* Button to add new journal entry */}
-  
- 
-   <NewJournalModal
+    <div>
+      {/* Button to add new journal entry */}
+      <NewJournalModal
         visible={modalVisible}
         onCreate={handleCreateJournal}
         onCancel={() => setModalVisible(false)}
-   />
-    <List
-      header={<div> <p>My Trading Journal 📝</p> <Button type="primary" style={{ marginBottom: '16px' }} onClick={() => setModalVisible(true)}>
-      Add Journal
-   </Button> </div>}
-      itemLayout="horizontal"
-      dataSource={journals}
-      renderItem={(item, index) => (
-        <List.Item key={index}>
-          <List.Item.Meta
-
-            avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
-            title={<Link to={`/journalDetails/${item.id}`}>{item.title}</Link>}
-            
-            description={
-              <div>
-                <p>Symbol: {item.symbol}</p>
-                <Tag><p> {item.buy_or_sell}</p></Tag>
-                <p>Experience: {item.experience}</p>
-                <p>Date: {item.date}</p>
-                <p>Created At: {item.created_at}</p>
-              </div>
-            }
-            
-          />
-          <p><strong>Symbol:</strong> {item.symbol}</p>
-          <Tag><p> {item.buy_or_sell}</p></Tag>
-          <p> {item.experience}</p>
-          <p>{item.date}</p>
-          
-        </List.Item>
-      )}
-    />
+      />
+      <List
+        header={<div> <p>My Trading Journal 📝</p> <Button type="primary" style={{ marginBottom: '16px' }} onClick={() => setModalVisible(true)}>
+          Add Journal
+        </Button> </div>}
+        itemLayout="horizontal"
+        dataSource={journals}
+        renderItem={(item, index) => (
+          <List.Item key={index}>
+            <List.Item.Meta
+              avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
+              title={<Link to={`/journalDetails/${item.id}`}>{item.title}</Link>}
+              description={
+                <div style={{ display: 'grid', gridTemplateColumns: 'auto auto', alignItems: 'center', gap: '8px' }}>
+                  <Flex>
+                  <div>
+                    
+                    <p><strong>Symbol:</strong> {item.symbol}</p>
+                    <Tag color={item.buy_or_sell === 'buy' ? '#108ee9' : '#f50'}>{item.buy_or_sell}</Tag>
+                  </div>
+                  <div>
+                    <p><strong>Experience:</strong></p>
+                    <div>
+                      {item.experience === 'happy' && <SmileOutlined style={{ color: 'green' }} />}
+                      {item.experience === 'neutral' && <MehOutlined style={{ color: 'grey' }} />}
+                      {item.experience === 'sad' && <FrownOutlined style={{ color: 'red' }} />}
+                    </div>
+                  </div>
+                  <div>
+                    <p><strong>Date:</strong> {item.date}</p>
+                    <p><strong>Created At:</strong> {item.created_at}</p>
+                  </div>
+                  </Flex>
+                </div>
+              }
+            />
+          </List.Item>
+        )}
+      />
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React,  { useState } from 'react';
-import {Menu} from 'antd';
+import {Menu, Dropdown} from 'antd';
 import{Link} from 'react-router-dom';
-import {CloudDownloadOutlined, TableOutlined,AreaChartOutlined,UserOutlined, ReadOutlined} from '@ant-design/icons';
+import {CloudDownloadOutlined, TableOutlined,AreaChartOutlined,UserOutlined, ReadOutlined, StopOutlined} from '@ant-design/icons';
 import  ImportComponent  from './Import';
 
 const Navbar = ({darkTheme}) => {
   const [isImportModalVisible, setIsImportModalVisible] = useState(false);
+  const [isProfileDropdownVisible, setIsProfileDropdownVisible] = useState(false);
 
   const handleOpenImportModal = () => {
     setIsImportModalVisible(true);
@@ -15,6 +16,25 @@ const Navbar = ({darkTheme}) => {
   const handleCloseImportModal = () => {
     setIsImportModalVisible(false);
   };
+
+  const handleProfileDropdownVisibleChange = (visible) => {
+    setIsProfileDropdownVisible(visible);
+  };
+
+  const profileMenu = (
+    <Menu>
+      <Menu.Item key="Profile" icon={<UserOutlined />}>
+        <Link to="/Profile">Profile</Link>
+      </Menu.Item>
+      <Menu.Item key="Profile" icon={<UserOutlined />}>
+        <Link to="/logout">Logout</Link>
+      </Menu.Item>
+      <Menu.Item key="Delete"  icon={<StopOutlined /> }>
+        <Link to="/delete">Delete</Link>
+      </Menu.Item>
+      {/* Add more menu items here if needed */}
+    </Menu>
+  );
   return (
         <>
         <Menu theme={darkTheme ? 'dark' : 'light'} mode="inline" className='menu-bar'>
@@ -30,9 +50,16 @@ const Navbar = ({darkTheme}) => {
           <Menu.Item key='Journal' icon={<ReadOutlined />}>
               <Link to="/Journal">Journal</Link>
             </Menu.Item>
-            <Menu.Item key='Profile' icon={<UserOutlined />}>
-              <Link to="/Profile">Profile</Link>
-            </Menu.Item>
+            <Dropdown
+          overlay={profileMenu}
+          placement="bottomRight"
+          visible={isProfileDropdownVisible}
+          onVisibleChange={handleProfileDropdownVisibleChange}
+        >
+          <Menu.Item key='Profile' icon={<UserOutlined />}>
+            Profile
+          </Menu.Item>
+        </Dropdown>
         </Menu>
         
         <ImportComponent visible={isImportModalVisible} onClose={handleCloseImportModal} />
